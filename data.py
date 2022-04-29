648d18c55fd715b3434cbce82b25ad5bbd912d56
johncarter371 <80877558+johncarter371@users.noreply.github.com>
2022-04-29 07:54:47 +0530

    Update data.py

--- data.py
+++ data.py
@@ -6,9 +6,9 @@ class Data:
     START = """
 𝐇𝐞𝐥𝐥𝐨 𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐖𝐢𝐜𝐤 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐁𝐨𝐭
 
-𝐖𝐞𝐥𝐜𝐨𝐦𝐞 {}
+𝐖𝐞𝐥𝐜𝐨𝐦𝐞 {} 𝐈'𝐦 𝐖𝐢𝐜𝐤 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐁𝐨𝐭.
 
-𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐖𝐢𝐜𝐤 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐨𝐫.
+𝐈 𝐚𝐦 𝐡𝐞𝐫𝐞 𝐭𝐨 𝐡𝐞𝐥𝐩 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞 𝐭𝐡𝐞 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐟𝐨𝐫 𝐲𝐨𝐮.
 𝐈 𝐜𝐚𝐧 𝐠𝐞𝐧𝐞𝐫𝐚𝐭𝐞 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 & 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐟𝐨𝐫 𝐲𝐨𝐮 𝐢𝐧 𝐞𝐚𝐬𝐲 𝐰𝐚𝐲
 
 𝐘𝐨𝐮 𝐜𝐚𝐧 𝐮𝐬𝐞 𝐦𝐞 𝐭𝐨 𝐠𝐞𝐧𝐞𝐫𝐚𝐭𝐞 𝐩𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐚𝐧𝐝 𝐭𝐞𝐥𝐞𝐭𝐡𝐨𝐧 𝐬𝐭𝐫𝐢𝐧𝐠 𝐬𝐞𝐬𝐬𝐢𝐨𝐧. 𝐔𝐬𝐞 𝐛𝐞𝐥𝐨𝐰 𝐛𝐮𝐭𝐭𝐨𝐧𝐬 ❗
@@ -50,7 +50,7 @@ class Data:
 
 A telegram bot to generate pyrogram and telethon string session by Wick
 
-Source Code : [Click Here](https://github.com/johncarter371/StringSessionBot)
+Source Code : [Click Here](https://github.com/johncarter371/genstring)
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
